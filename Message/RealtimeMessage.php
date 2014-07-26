1578646d57ba7b3672d15d0cb5c66254e4517a66
cnsaturn <yangg.hu@gmail.com>
2014-07-26 20:13:51 +0800

    Squashed 'lib/Yoozi/Notification/' changes from 6fd83d6..0974617
    0974617 Abstract Message.
    
    git-subtree-dir: lib/Yoozi/Notification
    git-subtree-split: 09746175a5ab40db181cc6106c7f15ede5a2d216

--- Message/RealtimeMessage.php
+++ Message/RealtimeMessage.php
@@ -7,53 +7,7 @@
  *  @author     Saturn <yangg.hu@yoozi.cn>
  *  @copyright  2013 Yoozi Information Technology Co., Ltd.
  */
-class RealtimeMessage extends Message {
-
-    /**
-     * Holds the attributes of this message.
-     *
-     * @var mixed
-     */
-    protected $attributes = array();
-
-    /**
-     * Set the sender who triggers this transport.
-     *
-     * @param  mixed $from
-     * @return \Yoozi\Notification\Message
-     */
-    public function from($from)
-    {
-        $this->attributes['from'] = $from;
-
-        return $this;
-    }
-
-    /**
-     * Set the recipient who we transport the message to.
-     *
-     * @param  mixed $to
-     * @return \Yoozi\Notification\Message
-     */
-    public function to($to)
-    {
-        $this->attributes['to'] = $to;
-
-        return $this;
-    }
-
-    /**
-     * Set the subject contained in the transport.
-     *
-     * @param  mixed $subject
-     * @return \Yoozi\Notification\Message
-     */
-    public function subject($subject)
-    {
-        $this->attributes['subject'] = $subject;
-
-        return $this;
-    }
+class RealtimeMessage extends AbstractMessage {
 
     /**
      * Set the main content contained in the transport.
@@ -68,7 +22,11 @@ class RealtimeMessage extends Message {
             // Since we deliver this message by POSTing it to the Real-time API, 
             // we have to make sure the message body comply the rules specified by
             // the HTTP protocol.
-            $content = array_map('object_to_array', $content);
+            $content = array_map(function ($value) {
+                if (is_object($value)) {
+                    return method_exists($value, 'toArray') ? $value->toArray() : (array) $value;
+                }
+            }, $content);
 
             $content = json_encode($content);
         }
